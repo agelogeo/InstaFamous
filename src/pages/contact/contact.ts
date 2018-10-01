ec565f24bc1b7f345e449f9d30a99398f35e2185
Giorgos Angelopoulos <agelogeo@gmail.com>
2018-10-01 22:57:30 +0300

    Improved a bit the store page

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -8,6 +8,30 @@ import {MyAccountProvider} from "../../providers/my-account/my-account";
 })
 export class ContactPage {
 
+  public cards = [
+    {
+      id: 1,
+      header: 'Starter Pack',
+      text: 'Start boosting your profile with 25 likes.',
+      price: 0.75,
+      likes: 25
+    },
+    {
+      id: 2,
+      header: 'Premium Pack',
+      text: 'Follow up with the premium pack of 100 likes.',
+      price: 2.5,
+      likes: 100
+    },
+    {
+      id: 3,
+      header: 'Pro Pack',
+      text: 'Every pro needs this pack of 250 likes.',
+      price: 5.00,
+      likes: 250
+    },
+   ];
+
   constructor(public myaccount : MyAccountProvider,public navCtrl: NavController) {
 
   }
